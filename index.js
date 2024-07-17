3e2d9e767c865e9f35597b50591c778e0d1702b4
Cameron Adams <camgadams@gmail.com>
2024-07-17 13:42:12 +0200

    fix:test if build will work on vercel

--- index.js
+++ index.js
@@ -1,44 +1,30 @@
-require("dotenv").config();
+const chromium = require('chrome-aws-lambda');
 
-const app = require("express")();
-
-let chrome = {};
-let puppeteer;
-
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  chrome = require("chrome-aws-lambda");
-  puppeteer = require("puppeteer-core");
-} else {
-  puppeteer = require("puppeteer");
-}
-
-app.get("/api", async (req, res) => {
-  let options = {};
-
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    options = {
-      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-      defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath,
-      headless: true,
-      ignoreHTTPSErrors: true,
-    };
-  }
+exports.handler = async (event, context, callback) => {
+  let result = null;
+  let browser = null;
 
   try {
-    let browser = await puppeteer.launch(options);
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
 
     let page = await browser.newPage();
-    await page.goto("https://www.google.com");
-    res.send(await page.title());
-  } catch (err) {
-    console.error(err);
-    return null;
+
+    await page.goto(event.url || 'https://example.com');
+
+    result = await page.title();
+  } catch (error) {
+    return callback(error);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
   }
-});
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server started");
-});
-
-module.exports = app;
+  return callback(null, result);
+};
